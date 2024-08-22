e76a6cab220beafd3d347affd93b54727460d364
Sarfaraz Qadir <sarfarazqadir594@gmail.com.com>
2024-08-23 00:58:04 +0500

    Add project files.

---
+++ PlantNest-Contest-E-Azam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlantNest_Contest_E_Azam.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ PlantNest-Contest-E-Azam/Models/myContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PlantNest_Contest_E_Azam.Models
+{
+	public class myContext : DbContext
+	{
+        public myContext(DbContextOptions options) : base (options)
+        {
+                
+        }
+    }
+}
